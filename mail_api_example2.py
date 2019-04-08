50f38a617c5afb1c0f97b663fa30c3832440317f
Missnadeline <48262214+Missnadeline@users.noreply.github.com>
2019-04-08 11:08:16 +0100

    Add files via upload

---
+++ mail_api_example2.py
@@ -0,0 +1,32 @@
+# Don't forget to "pip install requests"
+# Could be "sudo pip install request"
+import requests
+
+# Get this from http://app.mailgun.com/app/dashboard
+sender = "nadelina" #sender name is the name it will come from like nadelina@jeganathan.co.uk 
+domain_name = "sandbox1d4c0da5cdf34bfba2a2a8b53e863fbc.mailgun.org"
+api_key = "d4ebbbdc4a378933e8c13e8a2eacf679-985b58f4-3091d3f8"
+
+def send_simple_message(sendto,contact,message):
+    return requests.post(
+            "https://api.mailgun.net/v3/{0}/messages".format(domain_name),
+            auth=("api",api_key),  # authorisation token 
+            data={
+                    "from": "Nadleina <{0}@{1}>".format(sender, domain_name),
+                    "to": [sendto],
+                    "subject": "Alert email",
+                    "text": message + '/n Call me back on ' + str(contact) + 'please!'
+                },
+                    verify='C:\Users\georgin\Desktop\Python\zscaler.cer'
+            )
+    
+sendto = 'robertolivercollins@gmail.com'
+contact = '07525056730'
+message = 'My husband is beating me!'
+response = send_simple_message(sendto,contact,message)
+print response.url
+print response.status_code
+print response.headers["content-type"]
+print response.text
+                    
+                    
